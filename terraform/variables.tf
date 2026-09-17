d217f89a0855b2b10fd26f143af9eafa8305616e
Feroz Shaikh <firieshaikh@gmail.com>
2026-09-17 13:02:05 +0530

    Adding 1st TF resource the lambda IAM exec role

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "project_prefix" {
+  description = "Prefix for project resources"
+  type        = string
+  default     = "vpc-manager"
+}
