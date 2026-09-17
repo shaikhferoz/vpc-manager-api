8bf27c8e5b20bf2fc74b5b95c8b4c2362ff9920a
Feroz Shaikh <firieshaikh@gmail.com>
2026-09-17 15:53:35 +0530

    better commenting

--- terraform/modules/api_gateway/api_gateway.tf
+++ terraform/modules/api_gateway/api_gateway.tf
@@ -1,3 +1,5 @@
+# The API Gateway HTTP API.
+# TBD: No authorizers yet, will add later.
 resource "aws_apigatewayv2_api" "api" {
   name          = "${var.project_prefix}-api"
   protocol_type = "HTTP"
